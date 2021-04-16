e731a80961b2b152d59a16d55b98f523a0481101
RaaziaT <raazia01@gmail.com>
2021-04-16 23:26:10 +0500

    intents

--- app/src/main/java/com/raaziat/sampleapplication/MainActivity.java
+++ app/src/main/java/com/raaziat/sampleapplication/MainActivity.java
@@ -1,11 +1,23 @@
 package com.raaziat.sampleapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.databinding.DataBindingUtil;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Toast;
 
 import com.raaziat.sampleapplication.databinding.ActivityMainBinding;
+
+import java.security.Permission;
+import java.security.Permissions;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +26,47 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        activityMainBinding.viewBack.setOnClickListener(v -> onBackPressed());
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.CAMERA},
+                    1);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1) {
+            if (grantResults != null) {
+                if (grantResults.length <= 0) {
+                    Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(cameraIntent, 1);
+                    } else
+                        Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show();
+                }
+            } else Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1){
+
+
+        }
 
     }
 }
