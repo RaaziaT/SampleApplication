842ade4ce05e23b2ea914042bfb4ea0ab547630f
RaaziaT <raazia01@gmail.com>
2021-04-16 23:27:00 +0500

    intents

---
+++ app/src/main/java/com/raaziat/sampleapplication/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.raaziat.sampleapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.raaziat.sampleapplication.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding activityLoginBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+
+        activityLoginBinding.buttonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show();
+
+    }
+}
