5043d064dcb922b9439c10c9019f3c91c316be6a
RaaziaT <raazia01@gmail.com>
2021-04-19 23:08:54 +0500

    data binding click listener

---
+++ app/src/main/java/com/raaziat/sampleapplication/GotoSecondScreen.java
@@ -0,0 +1,5 @@
+package com.raaziat.sampleapplication;
+
+public interface GotoSecondScreen {
+    void openSecondScreen();
+}
--- app/src/main/java/com/raaziat/sampleapplication/LoginActivity.java
+++ app/src/main/java/com/raaziat/sampleapplication/LoginActivity.java
@@ -5,12 +5,11 @@ import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
 
 import com.raaziat.sampleapplication.databinding.ActivityLoginBinding;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements GotoSecondScreen {
 
     ActivityLoginBinding activityLoginBinding;
 
@@ -19,15 +18,23 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         activityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
 
-        activityLoginBinding.buttonSignup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
+//        activityLoginBinding.buttonSignup.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
+        activityLoginBinding.setClickInterface(this);
+        activityLoginBinding.setSetButtonText(getString(R.string.create_account));
         Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show();
 
+    }
+
+    @Override
+    public void openSecondScreen() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
