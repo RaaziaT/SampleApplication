3cc7ecb9cce53084330ae1892b32c7d15408c2b3
RaaziaT <raazia01@gmail.com>
2021-04-22 23:51:47 +0500

    imageView explained

---
+++ app/src/main/java/com/raaziat/sampleapplication/SampleLoginActivity.java
@@ -0,0 +1,14 @@
+package com.raaziat.sampleapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SampleLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_login);
+    }
+}
